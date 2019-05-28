4a25a9da69c8421c141265ba3fff81f2a52c8109
Anna Beagley <beagley.dev@gmail.com>
2019-05-28 11:30:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var property= document.getElementById(btn);
+if (count == 0){
+    document.body.style= "background-color: #111111 "
+    count=1;
+} else {
+    document.body.style="background-image:url(https://media.giphy.com/media/U3qYN8S0j3bpK/source.gif)"
+    count=0;
+}
